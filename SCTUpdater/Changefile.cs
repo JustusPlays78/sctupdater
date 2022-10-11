a773c29ddd8ae9c41660603155fae412e01d49a7
Julian <JustusPlays78@gmail.com>
2022-10-11 16:17:33 +0200

    first commit

---
+++ SCTUpdater/Changefile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SCTUpdater
+{
+    internal class Changefile
+    {
+        private static String path;
+
+        public static void startbutton(Boolean cidname, Boolean password)
+        {
+
+        }
+
+        private static void readtxt()
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(path);
+                string txt = reader.ReadLine();
+
+            }
+            catch
+            {
+                Console.Write("Execption");
+            }
+
+
+
+        } 
+    }
+}
---
+++ SCTUpdater/Jsonfile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SCTUpdater
+
+{
+    internal class Jsonfile
+    {
+        
+
+        public static void savecreds(string nametext, string passwordtext, string cpdlctext)
+        {
+            string path = @"C:\Users\Julian\Desktop\sctshit\creds.json";
+
+                creds cred = new creds { cidname = nametext, password = passwordtext, cpdlc = cpdlctext };
+                string JSONresult = JsonConvert.SerializeObject(cred); 
+                File.WriteAllText(path,JSONresult);
+                /*using (var js = new StreamWriter(path, true))
+                {
+                    js.WriteLine(JSONresult);
+                    js.Close();
+                }*/
+
+        }
+
+    }
+
+    internal class creds
+    {
+        public string cidname;
+        public string password;
+        public string cpdlc;
+    }
+}
