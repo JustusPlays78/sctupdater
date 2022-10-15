5e7a22975506aa2f2394698e9431d8743ed58c2f
Julian <JustusPlays78@gmail.com>
2022-10-15 15:47:57 +0200

    Fix .NET 6.0 didnt work -> Downgrade to 5.0

--- SCTUpdater/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ SCTUpdater/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -190,7 +190,7 @@ namespace SCTUpdater {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SCTUpdater;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SCTUpdater;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
