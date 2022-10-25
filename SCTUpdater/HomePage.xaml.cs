7b737bfe6962912a84ab4045865145f6181e312a
Julian <JustusPlays78@gmail.com>
2022-10-25 22:32:32 +0200

    New UI :100:

---
+++ SCTUpdater/HomePage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SCTUpdater
+{
+    /// <summary>
+    /// Interaction logic for Ersatzwindow.xaml
+    /// </summary>
+    public partial class HomePage : Window
+    {
+        public HomePage()
+        {
+        
+        InitializeComponent();
+        }
+
+        private void MouseDragAndDrop(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void HomeTabClose(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private void HomeTabMinimize(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+    }
+}
