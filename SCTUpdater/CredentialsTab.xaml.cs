de46f5a15fea4b5c6b72ece86de396045b9c8086
Julian <JustusPlays78@gmail.com>
2022-11-03 13:08:41 +0100

    metashit

--- SCTUpdater/CredentialsTab.xaml.cs
+++ SCTUpdater/CredentialsTab.xaml.cs
@@ -10,24 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SCTUpdater
 {
-
-    public partial class HomePage : Window
+    /// <summary>
+    /// Interaction logic for CredentialsTab.xaml
+    /// </summary>
+    public partial class CredentialsTab : Window
     {
-        public HomePage()
+        public CredentialsTab()
         {
-        
-        InitializeComponent();
+            InitializeComponent();
         }
-
         private void MouseDragAndDrop(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
         }
-
         private void HomeTabClose(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
@@ -36,11 +36,6 @@ namespace SCTUpdater
         {
             this.WindowState = WindowState.Minimized;
         }
-
-        private void HomeTab_Click(object sender, RoutedEventArgs e)
-        {
-            HomePage CredentialsTab = new HomePage();
-            this.Content = CredentialsTab;
-        }
     }
+    
 }
