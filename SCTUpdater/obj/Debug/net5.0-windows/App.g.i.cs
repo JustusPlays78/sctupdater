2a6f6563db44b3fdead024fe5d65a5e3136e9ecf
Julian <JustusPlays78@gmail.com>
2023-10-04 15:29:51 +0200

    123

--- SCTUpdater/obj/Debug/net5.0-windows/App.g.i.cs
+++ SCTUpdater/obj/Debug/net5.0-windows/App.g.i.cs
@@ -46,7 +46,7 @@ namespace SCTUpdater {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -61,7 +61,7 @@ namespace SCTUpdater {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public static void Main() {
             SCTUpdater.App app = new SCTUpdater.App();
             app.InitializeComponent();
